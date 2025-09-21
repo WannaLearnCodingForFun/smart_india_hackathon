b8583956f4d5df478ee55ffd30318686b9e08395
Atharva Bandekar <atharvabandekar13@gmail.com>
2025-09-21 14:47:15 +0530

    Basic Core Data Model

---
+++ core_data_model.js
@@ -0,0 +1,28 @@
+{
+  "resourceType": "CollectionEvent",
+  "id": "CE-ASH-20250921-0001",
+  "timestamp": "2025-09-21T06:12:33+05:30",
+  "collector": {
+    "id": "COL-1001",
+    "name": "R. Kumar",
+    "organization": "VillageCoopA"
+  },
+  "species": {
+    "latin": "Withania somnifera",
+    "common": "Ashwagandha",
+    "batchTag": "BATCH-ASH-0001"
+  },
+  "location": {
+    "lat": 20.123456,
+    "lon": 73.987654,
+    "accuracyMeters": 8
+  },
+  "weightKg": 12.4,
+  "initialQuality": {
+    "moisturePct": 9.8,
+    "notes": "Leaves trimmed, no visible mold"
+  },
+  "photoUrl": "s3://minio/ash/CE-ASH-20250921-0001.jpg",
+  "geoHash": "mnjk9..." ,
+  "signature": "BASE64_SIGNATURE_BY_COLLECTOR"
+}
